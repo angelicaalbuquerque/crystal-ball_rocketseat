d6e1ee222d157b36e70b65d0d8453841cde79bc9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-06-12 13:54:29 -0300

    Checagem de quantidade de resposta e botão funcional

---
+++ script.js
@@ -0,0 +1,42 @@
+/* 
+Quais são os meus passos?
+- Definir quais são as minhas variáveis
+- Definir quais são meus dados de entrada
+- Definir quais são meus dados de saída
+
+ 1 - Ver quantidade de respostas;
+ 2 - Gerar número aleatório;
+ 3 - Tornar botão funcional;
+ 4 - Fazer a resposta aparecer em tela;
+ */
+
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+];
+
+//clicar em fazer pergunta e executar pedaço de código
+function fazerPergunta() {
+  //gerar número aleatório
+  const totalRespostas = respostas.length;
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
+
+  console.log(respostas[numeroAleatorio]);
+}
